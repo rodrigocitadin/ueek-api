6efe672a00b08b4295ffc1442224255c2df37e3e
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-20 16:45:36 -0300

    create users, address, numbers and transactions migrations

---
+++ database/migrations/2023_12_20_192635_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount');
+            $table->boolean('is_paid');
+
+            $table->foreignId('users_id')->constrained();
+            $table->foreignId('numbers_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
