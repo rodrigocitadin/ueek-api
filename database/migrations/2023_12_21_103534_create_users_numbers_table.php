26e8fce45c20b031fd481488618545aab8492105
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-21 07:40:28 -0300

    create an additional table to connect users and numbers

--- database/migrations/2023_12_21_103534_create_users_numbers_table.php
+++ database/migrations/2023_12_21_103534_create_users_numbers_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create('users_numbers', function (Blueprint $table) {
             $table->id();
-            $table->decimal('amount');
-            $table->boolean('is_paid');
+            $table->foreignId('users_id')->constrained();
+            $table->foreignId('numbers_id')->constrained();
+            $table->foreignId('transactions_id')->constrained();
 
             $table->timestamps();
         });
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('users_numbers');
     }
 };
