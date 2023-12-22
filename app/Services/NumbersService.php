ce92f11ceb28c2135001ba3d8d92bd1a1a7d4486
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-22 18:16:34 -0300

    remove create from numbers

--- app/Services/NumbersService.php
+++ app/Services/NumbersService.php
@@ -20,13 +20,6 @@ class NumbersService
         return $numbers;
     }
 
-    public static function create($data)
-    {
-        $numbers = Numbers::create($data);
-
-        return $numbers;
-    }
-
     public static function update($data, $id)
     {
         $numbers = NumbersService::getById($id)->update($data);
