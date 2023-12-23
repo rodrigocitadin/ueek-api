cea1c79644ec9c852ba13b26d3e700af8b98da01
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-23 15:19:51 -0300

    finishing seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(NumbersSeeder::class);
+        $this->call(AddressSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(TransactionSeeder::class);
     }
 }
